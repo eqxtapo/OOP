bc8f41a972329932a404fc33010875058eb086ce
unknown <extapo@yandex>
2025-02-21 18:14:08 +0700

    add new fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,41 +21,36 @@ def product_xiaomi():
 
 
 @pytest.fixture()
-def category1():
-    product1 = 1
-    product2 = 2
-    product3 = 3
+def category_phones():
     return Category(
-        "Смартфоны",
-        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-        [product1, product2, product3],
+        name="Смартфоны",
+        description="Смартфоны, как средство не только коммуникации, но и получения дополнительных "
+                    "функций для удобства жизни",
+        products=[
+            Product(
+                "Samsung Galaxy S23 Ultra",
+                "256GB, Серый цвет, 200MP камера",
+                180000.0,
+                5,
+            ),
+            Product("Iphone 15", "512GB, Gray space", 210000.0, 8),
+            Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14),
+        ],
     )
 
 
 @pytest.fixture()
-def category1_counter():
-    product1 = 1
-    product2 = 2
-    product3 = 3
-
+def category_tv():
     return Category(
-        "Смартфоны",
-        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-        [product1, product2, product3],
+        name="Телевизоры",
+        description="Современный телевизор, который позволяет наслаждаться просмотром, "
+                    "станет вашим другом и помощником",
+        products=[
+            Product(
+                "Samsung",
+                "QLED 4K, Фоновая подсветка",
+                123000.0,
+                7,
+            ),
+        ],
     )
-
-
-@pytest.fixture()
-def category2():
-    product4 = 4
-    product5 = 5
-    return Category(
-        "Телевизоры",
-        "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
-        [product4, product5],
-    )
-
-
-@pytest.fixture()
-def product_category_count():
-    return
