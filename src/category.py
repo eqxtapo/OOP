bd1ff2166b8fdd4e86f90a6aeb7c666d5eaaaeb1
unknown <extapo@yandex>
2025-02-23 15:58:15 +0700

    add new method's

--- src/category.py
+++ src/category.py
@@ -17,6 +17,9 @@ class Category(Product):
         Category.category_count += 1
         Category.product_count += len(products)
 
+    def __str__(self):
+        return f"{self.name}, количество продуктов: {self.products_counter()} шт."
+
     def add_product(self, product: Product) -> Any:
         if issubclass(Category, Product):
             self.__products.append(product)
@@ -30,5 +33,11 @@ class Category(Product):
     def products(self):
         product_str = ""
         for product in self.__products:
-            product_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+            product_str += f"{str(product)}\n"
         return product_str
+
+    def products_counter(self):
+        products_counter = 0
+        for product in self.__products:
+            products_counter += product.quantity
+        return products_counter
--- src/product.py
+++ src/product.py
@@ -10,6 +10,12 @@ class Product:
         self.__price = price
         self.quantity = quantity
 
+    def __str__(self):
+        return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
+
+    def __add__(self, other):
+        return (self.quantity * self.__price) + (other.quantity * other.__price)
+
     @classmethod
     def new_product(cls, new_product: dict):
         name = new_product["name"]
