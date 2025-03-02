1ffab5f38f4afe37ae989c993dcd28fd01b0cbea
unknown <extapo@yandex>
2025-03-02 13:52:16 +0700

    fixes

--- src/product.py
+++ src/product.py
@@ -1,4 +1,8 @@
-class Product:
+from src.baseproduct import BaseProduct
+from src.mixin_print import Mixin_print
+
+
+class Product(BaseProduct, Mixin_print):
     name: str
     description: str
     price: float
@@ -9,6 +13,7 @@ class Product:
         self.description = description
         self.__price = price
         self.quantity = quantity
+        super().__init__()
 
     def __str__(self):
         return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
