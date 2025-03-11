a306b23a4b06659ac1d343579d46fdbb89d16912
unknown <extapo@yandex>
2025-03-11 18:26:59 +0700

    add exception in init

--- src/product.py
+++ src/product.py
@@ -12,6 +12,8 @@ class Product(BaseProduct, Mixin_print):
         self.name = name
         self.description = description
         self.__price = price
+        if quantity == 0:
+            raise ValueError("Товар с нулевым количеством не может быть добавлен")
         self.quantity = quantity
         super().__init__()
 
