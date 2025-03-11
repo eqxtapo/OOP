e0a1467c1540d308c91c1bb0c2b684b50e63f925
unknown <extapo@yandex>
2025-03-11 18:27:35 +0700

    add new tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -115,3 +115,12 @@ def grass():
         "7 дней",
         "Зеленый",
     )
+
+
+@pytest.fixture
+def product_invalid():
+    return Product("Бракованный товар", "Неверное количество", 1000.0, 0)
+
+@pytest.fixture
+def category_empty():
+    return Category("Пустая категория", "Категория без продуктов", [])
